60660db4d2ac4732cc5836eeebe9bcaedebc3424
Aditya <lapratomo24@yahoo.com>
2024-09-10 20:02:45 +0200

    used list comprehension to convert name input to phonetic code

--- main.py
+++ main.py
@@ -21,13 +21,10 @@ for (index, row) in student_data_frame.iterrows():
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
+new_dict = {row.letter: row.code for index, row in data.iterrows()}
+# print(new_dict)
 
-converted = {row.letter: row.code for (index, row) in data.iterrows()}
-print(converted)
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("What is your name? ")
+user_input = input("Enter a name: ").upper()
+phonetic_code = [new_dict[letter] for letter in user_input]
+print(phonetic_code)
